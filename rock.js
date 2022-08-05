fa1c82d02d984156d17c44e4cd02d80903d2d435
dokugo90 <dokugo90@gmail.com>
2022-08-05 13:49:14 +0000

    Console

--- rock.js
+++ rock.js
@@ -10,13 +10,13 @@ function getComputerChoice() {
     let random = Math.floor(Math.random() * 3)
 
     if (random == 0) {
-        return 'rock'
+        return 'rock' 
     } else if (random == 1) {
         return 'paper'
     } else if (random == 2) {
         return 'scissors'
     } else {
-        return 'error'
+        return 'Computer error please try again'
     }
 };
 
@@ -26,15 +26,27 @@ function getWinner(userChoice, computerChoice) {
         return 'It is a tie!'
     } else if (userChoice == 'rock' && computerChoice == 'paper') {
         return 'Computer won!'
+    } else if (userChoice == 'Rock' && computerChoice == 'paper') {
+        return 'Computer won!'
     } else if (userChoice == 'paper' && computerChoice == 'rock') {
         return 'You won!'
+    } else if (userChoice == 'Paper' && computerChoice == 'rock') {
+        return 'You won!'
+    } else if (userChoice == 'Scissors' && computerChoice == 'rock') {
+        return 'Computer won!'
     } else if (userChoice == 'scissors' && computerChoice == 'rock') {
         return 'Computer won!'
+    } else if (userChoice == 'Rock' && computerChoice == 'scissors') {
+        return 'You won!'
     } else if (userChoice == 'rock' && computerChoice == 'scissors') {
         return 'You won!'
     } else if (userChoice == 'paper' && computerChoice == 'scissors') {
         return 'Computer won!'
+    } else if (userChoice == 'Paper' && computerChoice == 'scissors') {
+        return 'Computer won!'
     } else if (userChoice == 'scissors' && computerChoice == 'paper') {
+        return 'You won!'
+    } else if (userChoice == 'Scissors' && computerChoice == 'paper') {
         return 'You won!'
     } else if (userChoice == 'duke') {
         return 'Congrats on discovering the cheat code. You won!'
@@ -42,10 +54,10 @@ function getWinner(userChoice, computerChoice) {
 }
 
 const playGame = () => {
-    let userChoice = getUserChoice('');
+/*Enter rock, paper, scissors here in lower case.*/ let userChoice = getUserChoice('').toLowerCase();
     const computerChoice = getComputerChoice();
     console.log(`You played: ${userChoice}.`);
-    console.log(`The Computer played: ${computerChoice}`);
+    console.log(`The Computer played: ${computerChoice}.`);
     console.log(getWinner(userChoice, computerChoice));
 
 }
